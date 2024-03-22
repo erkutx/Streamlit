63a236c72c07bbcc68759d8b09103a006200ef27
Erkut Cetiner <erkutcetiner0@gmail.com>
2024-03-22 17:54:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,21 @@ import streamlit as st # pip install streamlit
 import plotly.grapgh_objects as go # pipi install plotly 
 
 
-# Settings --------------
+# ---------------- Settings -----------------
+
+incomes = ["Salary", "Blog" , "Other Income" ]
+expenses = ["Rent","Utilities","Groceries","Car","Other Exxpenses", "Savings"]
+currency = "USD"
+page_title = 'Income  and Expense Tracker'
+page_icon = ":money with wings:"
+layout = 'centered'
+
+# ---------------------
+
+st.set_page_config (page_title-page_title, page_icon-page_icon, layout=layout) 
+
+
+
 
 
 
