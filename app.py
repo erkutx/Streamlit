6aa6ef5aee186e9a09d635f2c65a070da14953f2
Erkut Cetiner <erkutcetiner0@gmail.com>
2024-03-23 17:37:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime # from python core
 # ---------------- Settings -----------------
 
 incomes = ["Salary", "Blog" , "Other Income" ]
-expenses = ["Rent","Utilities","Groceries","Car","Other Exxpenses", "Savings"]
+expenses = ["Rent","Utilities","Groceries","Car","Other Expenses", "Savings"]
 currency = "USD"
 page_title = 'Income  and Expense Tracker'
 page_icon = ":money_with_wings:"
@@ -22,11 +22,30 @@ st.title(page_title + " " + page_icon)
 
 # ---------- DROPDOWN VALUES FOR SELECTION ------------
 
-years = [datetime.today().year , datetime.today().year + 1]]
+years = [datetime.today().year , datetime.today().year + 1]
 months = list(calendar.month_name[1:])
 
-# ---------- INPUTs & SAVEs ------------
 
+
+# ---------- INPUT & SAVE PERIOD ------------
+
+
+st.header(f"Data Entry in {currency}")
+with st.form("entry_form",clear_on_submit = True):
+  col1, col2 = st.columns(2)
+  col1.selectbox("Select Month:", months , key = "month")
+  col2.selectbox("Select Year:", years, key="year")
+
+
+"---"
+
+with st.expander("Income"):
+  for income in incomes:
+    st.number_input(f"{income}:", min_value=0, format= "%i",step =10, key=income)
+
+with st.expander("Expenses"):
+  for expense in expenses:
+    st.number_input(f"{expense}:", min_value=0, format="%i",step=10, key=expense) 
 
 
 
