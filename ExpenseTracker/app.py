f54a23b4e8ab36e3850c3127dc8dbfe7670c01a5
Erkut Cetiner <erkutcetiner0@gmail.com>
2024-03-27 16:09:45 -0400

    resolved

--- ExpenseTracker/app.py
+++ ExpenseTracker/app.py
@@ -47,7 +47,7 @@ with st.form("entry_form", clear_on_submit=True):
     "---"
     submitted = st.form_submit_button("Save Data")
     if submitted:
-        period = str(st.session_state["year"]) + "_" + (st.session_state["month"])
+        period = str(selected_year) + "_" + (selected_month)
         income_values = {income: st.session_state[income] for income in incomes}
         expense_values = {expense: st.session_state[expense] for expense in expenses}
 
@@ -56,40 +56,39 @@ with st.form("entry_form", clear_on_submit=True):
         st.write(f"expenses: {expense_values}")
         st.success("Data saved!")
 
-        # ----------- PLOT PERIODS -------------
+# ----------- PLOT PERIODS -------------
 
-        st.header("Data Visualization")
-        with st.form("saved_periods"):
-            # TODO: Get periods from db
-            selected_period = st.selectbox("Select Period", ["2022_March"])
-            submitted = st.form_submit_button("Plot Period")
-            if submitted:
-                # TODO: Get Data from database
-                comment = "Some comment"
-                incomes = {'Salary': 1500, 'Blog': 50, 'Other Income': 10}
-                expenses = {'Rent': 600, 'Utilities': 200, 'Groceries': 300, 'Car': 100, 'Other Expenses': 50, 'Savings': 10}
+st.header("Data Visualization")
+# TODO: Get periods from db
+selected_period = st.selectbox("Select Period", ["2022_March"])
+submitted = st.button("Plot Period")
+if submitted:
+    # TODO: Get Data from database
+    comment = "Some comment"
+    incomes = {'Salary': 1500, 'Blog': 50, 'Other Income': 10}
+    expenses = {'Rent': 600, 'Utilities': 200, 'Groceries': 300, 'Car': 100, 'Other Expenses': 50, 'Savings': 10}
 
-                # Create metrics
-                total_income = sum(incomes.values())
-                total_expense = sum(expenses.values())
-                remaining_budget = total_income - total_expense
-                col1, col2, col3 = st.columns(3)
-                col1.metric("Total Income", f"{total_income} {currency}")
-                col2.metric("Total Expense", f"{total_expense} {currency}")
-                col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
-                st.text(f"Comment: {comment}")
+    # Create metrics
+    total_income = sum(incomes.values())
+    total_expense = sum(expenses.values())
+    remaining_budget = total_income - total_expense
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Total Income", f"{total_income} {currency}")
+    col2.metric("Total Expense", f"{total_expense} {currency}")
+    col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
+    st.text(f"Comment: {comment}")
 
-                # Create sankey chart
-                label = list(incomes.keys()) + ["Total Income"] + list(expenses.keys())
-                source = list(range(len(incomes))) + [len(incomes)] * len(expenses)
-                target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses.keys()]
-                value = list(incomes.values()) + list(expenses.values())
+    # Create sankey chart
+    label = list(incomes.keys()) + ["Total Income"] + list(expenses.keys())
+    source = list(range(len(incomes))) + [len(incomes)] * len(expenses)
+    target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses.keys()]
+    value = list(incomes.values()) + list(expenses.values())
 
-                # Data to dict, dict to sankey
-                link = dict(source=source, target=target, value=value)
-                node = dict(label=label, pad=30, thickness=30, color="#E694FF")
-                data = go.Sankey(link=link, node=node)
-                # Plot it!
-                fig = go.Figure(data)
-                fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
-                st.plotly_chart(fig, use_container_width=True)
+    # Data to dict, dict to sankey
+    link = dict(source=source, target=target, value=value)
+    node = dict(label=label, pad=30, thickness=30, color="#E694FF")
+    data = go.Sankey(link=link, node=node)
+    # Plot it!
+    fig = go.Figure(data)
+    fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
+    st.plotly_chart(fig, use_container_width=True)
