5d5492c7dc450238a58b449cd4bc7a5f670cc59b
Erkut Cetiner <erkutcetiner0@gmail.com>
2024-03-26 20:26:00 -0400

    .

---
+++ ExpenseTracker/database.py
@@ -0,0 +1,8 @@
+from appwrite.client import Client
+from appwrite.services.databases import Databases
+from appwrite.id import ID
+import os
+from dotenv import load_dotenv
+import streamlit as st  # pip install streamlit
+
+
