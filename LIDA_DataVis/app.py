164dbf2d5ea7c7f7a8a4e0292197efedb6078cff
Erkut Cetiner <erkutcetiner0@gmail.com>
2024-04-03 19:01:53 -0400

    Summarizer added

--- LIDA_DataVis/app.py
+++ LIDA_DataVis/app.py
@@ -9,3 +9,30 @@ from PIL import Image
 from io import BytesIO
 import base64
 
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+def base64_to_image(base64_string):
+    # Decode the base64 string
+    byte_data = base64.b64decode(base64_string)
+    
+    # Use BytesIO to convert the byte data to image
+    return Image.open(BytesIO(byte_data))
+
+
+lida = Manager(text_gen = llm("openai"))
+textgen_config = TextGenerationConfig(n=1, temperature=0.5, model="gpt-3.5-turbo-0301", use_cache=True)
+
+menu = st.sidebar.selectbox("Choose an Option", ["Summarize", "Question based Graph"])
+
+if menu == "Summarize":
+    st.subheader("Summarization of your Data")
+    file_uploader = st.file_uploader("Upload your CSV", type="csv")
+    if file_uploader is not None:
+        path_to_save = "filename.csv"
+        with open(path_to_save, "wb") as f:
+            f.write(file_uploader.getvalue())
+        summary = lida.summarize("filename.csv", summary_method="default", textgen_config=textgen_config)
+        st.write(summary)
+        goals = lida.goals(summary, n=2, textgen_config=textgen_config)
+        for goal in goals:
